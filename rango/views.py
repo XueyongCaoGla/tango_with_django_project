d3232549bf86af9ef026f3abf7afbc5e15b52e22
“XueyongCaoGla” <2602134c@student.gla.ac.uk>
2021-07-30 07:23:25 +0800

    Before 9.7

--- rango/views.py
+++ rango/views.py
@@ -6,6 +6,7 @@ from rango.forms import CategoryForm
 from django.shortcuts import redirect
 from django.urls import reverse
 from rango.forms import PageForm
+from rango.forms import UserForm, UserProfileForm
 
 # Create your views here.
 def index(request):
@@ -87,3 +88,34 @@ def add_page(request, category_name_slug):
 #def get_category_list(current_category=None):
     #return {'categories': Category.objects.all(),
             #'current_category': current_category}
+            
+def register(request):
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileForm(request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user =user_form.save()
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+
+            profile.save()
+            registered = True
+        else:
+            print(user_form.errors, profile_form.errors)
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+    
+    return render(request, 'rango/register.html',
+                  context = {'user_form': user_form,
+                             'profile_form': profile_form,
+                             'registered': registered})
