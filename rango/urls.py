666dae312dc09c3893ac403f65c9533acf861cb9
“XueyongCaoGla” <2602134c@student.gla.ac.uk>
2021-07-29 23:02:10 +0800

    Completed Chapter6

--- rango/urls.py
+++ rango/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     #new path mapping to the about() view
     path('about/', views.about, name='about'),
+    path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
 ]
